01b9991e503fc84e87f828306bd768b4e6bfe351
ryeong <lrb9105@gmail.com>
2022-08-19 01:04:24 +0900

    주석 제거

--- app/src/main/java/com/example/bubbly/model/UserInfo.java
+++ app/src/main/java/com/example/bubbly/model/UserInfo.java
@@ -9,10 +9,7 @@ public class UserInfo {
     public static String profile_file_name = "";
     public static String user_nick = "";
     public static String self_info = "";
-<<<<<<< HEAD
     public static String token = "";
-=======
     public static String user_addr = "";
     public static String mnemonic = "";
->>>>>>> 8687c672b621be83d19ade343e0dbe796a3ff6a8
 }
