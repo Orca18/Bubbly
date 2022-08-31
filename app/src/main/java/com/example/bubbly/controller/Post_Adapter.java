afec1c4bb884a0ca8f0b8dc0be793944252e6a4c
Orca18 <lukedev1008@gmail.com>
2022-08-31 20:32:57 +0900

    0831 - TODO 리스트 남은거 노션 외 작업중

--- app/src/main/java/com/example/bubbly/controller/Post_Adapter.java
+++ app/src/main/java/com/example/bubbly/controller/Post_Adapter.java
@@ -167,7 +167,10 @@ public class Post_Adapter extends RecyclerView.Adapter<Post_Adapter.PostViewHold
         }
 
 
-
+        Glide.with(mContext)
+                .load("https://d2gf68dbj51k8e.cloudfront.net/" + post_response.getFile_save_names())
+                .fitCenter()
+                .into(holder.iv_media);
 
         holder.tv_user_id.setText(post_response.getLogin_id());
 //        holder.tv_com_name.setText(post_response.getCommunity_id());
